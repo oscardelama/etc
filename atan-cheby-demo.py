85216d1680d3be5d113f421945a997ad0c0fb120
Oscar de Lama <oscardelama@users.noreply.github.com>
2021-01-03 14:39:34 -0500

    Rename

--- atan-cheby-demo.py
+++ atan-cheby-demo.py
@@ -1,36 +1,36 @@
 from mpmath import mp, mpf
 mp.dps = 30
 
-def poly_cheb_odd(x):
-    poly_cheb_odd.p2 = 4*x*x-2
-    poly_cheb_odd.p_nm1 = x
-    poly_cheb_odd.p_nm2 = x
+def atan_poly_cheb(x):
+    atan_poly_cheb.p2 = 4*x*x-2
+    atan_poly_cheb.p_nm1 = x
+    atan_poly_cheb.p_nm2 = x
     yield x
     while True:
-        p_np1 = poly_cheb_odd.p2*poly_cheb_odd.p_nm1 \
-            - poly_cheb_odd.p_nm2
-        poly_cheb_odd.p_nm2 = poly_cheb_odd.p_nm1
-        poly_cheb_odd.p_nm1 = p_np1
+        p_np1 = atan_poly_cheb.p2*atan_poly_cheb.p_nm1 \
+            - atan_poly_cheb.p_nm2
+        atan_poly_cheb.p_nm2 = atan_poly_cheb.p_nm1
+        atan_poly_cheb.p_nm1 = p_np1
         yield p_np1
 
 
-def atan_cheby_coef():
+def atan_coef():
     SQRT2_M1 = mp.sqrt(2) - mpf(1)
     SQRT2_M1_2 = SQRT2_M1*SQRT2_M1
-    atan_cheby_coef.k = mpf(0)
-    atan_cheby_coef.rho_factor = mpf(-2)/SQRT2_M1
+    atan_coef.k = mpf(0)
+    atan_coef.rho_factor = mpf(-2)/SQRT2_M1
 
     while True:
-        atan_cheby_coef.k += mpf(1)
-        atan_cheby_coef.rho_factor *= - SQRT2_M1_2
-        coef = atan_cheby_coef.rho_factor/(2*atan_cheby_coef.k-1)
+        atan_coef.k += mpf(1)
+        atan_coef.rho_factor *= - SQRT2_M1_2
+        coef = atan_coef.rho_factor/(2*atan_coef.k-1)
         yield coef
 
 
 def main():
     result = 0
-    cheby = poly_cheb_odd(1)
-    coef = atan_cheby_coef()
+    cheby = atan_poly_cheb(1)
+    coef = atan_coef()
 
     for i in range(37):
         result += next(coef) * next(cheby)
@@ -46,4 +46,6 @@ main()
 # Pi Approx:  0.785398163397448309615660845821
 # Pi Exact :  3.14159265358979323846264338328
 # Pi Approx Error:  -2.35619449019234492884698253746
+
+
 
